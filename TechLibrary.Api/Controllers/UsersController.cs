fbe12bc2bbb0e39883a8808f5cb33d86a2d65814
Zenx007 <zenxpokas@gmail.com>
2025-06-08 21:50:58 -0300

    Fix features

---
+++ TechLibrary.Api/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechLibrary.Api.UseCases.Users.Register;
+using TechLibrary.Communitcation.Requests;
+using TechLibrary.Communitcation.Responses;
+
+namespace TechLibrary.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterUserJson), 201)]
+    public IActionResult Create(RequestUserJson request)
+    {
+
+        var useCase = new RegisterUserUseCase();
+
+        var response = useCase.Execute(request);
+
+
+        return Created(string.Empty, response);
+    }
+
+}
---
+++ TechLibrary.Api/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -0,0 +1,32 @@
+﻿using TechLibrary.Communitcation.Requests;
+using TechLibrary.Communitcation.Responses;
+using TechLibrary.Exception;
+
+namespace TechLibrary.Api.UseCases.Users.Register;
+
+public class RegisterUserUseCase
+{
+    private readonly List<string> errorMessages;
+
+    public ResponseRegisterUserJson Execute(RequestUserJson request)
+    {
+        Validate(request);
+        return new ResponseRegisterUserJson
+        {
+
+        };
+    }
+    private void Validate(RequestUserJson request)
+    {
+        var validator = new RegisterUserValidator();
+
+        var result = validator.Validate(request);
+
+        if (result.IsValid == false)
+        {
+            var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+        }
+
+        throw new ErrorOnValidationException(errorMessages);
+    }
+}
