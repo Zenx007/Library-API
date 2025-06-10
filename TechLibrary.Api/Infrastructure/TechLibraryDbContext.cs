61a66db3a8d883e0cb991487ff91950592679944
Zenx007 <zenxpokas@gmail.com>
2025-06-09 23:59:42 -0300

    DbContext

---
+++ TechLibrary.Api/Infrastructure/TechLibraryDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TechLibrary.Api.Infrastructure;
+
+public class TechLibraryDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }  
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source");
+    }
+}
