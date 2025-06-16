8eab9073918ba28d58de6e770f017815e9c367d1
Zenx007 <zenxpokas@gmail.com>
2025-06-15 23:45:36 -0300

    Feat LoginController

---
+++ TechLibrary.Api/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TechLibrary.Communication.Requests;
+
+namespace TechLibrary.Api.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class LoginController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult DoLogin(RequestLoginJson request)
+    {
+
+    }
+}
