7cc3ebe6e04291cf308a8edc8d578d4e8ff9f160
Zenx007 <zenxpokas@gmail.com>
2025-06-26 23:59:57 -0300

    Fix

--- TechLibrary.Api/Program.cs
+++ TechLibrary.Api/Program.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using TechLibrary.Api.Filters;
 
 const string AUTHENTICATION_TYPE = "Bearer"; 
@@ -28,31 +29,61 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidateIssuerSigningKey = true,
             IssuerSigningKey = SecurityKey()
         };
-    }); 
+    });
+ builder.Services.AddSwaggerGen(options =>
+    {
+     options.AddSecurityDefinition(AUTHENTICATION_TYPE, new OpenApiSecurityScheme
+     {
+         Description = @"JWT Authorization header using the Bearer scheme.
+                   Enter 'Bearer' [space] and then your token in the text input below.
+                   Example: 'Bearer 12345abcdef'",
+         Name = "Authorization",
+         In = ParameterLocation.Header,
+         Type = SecuritySchemeType.ApiKey,
+          Scheme = AUTHENTICATION_TYPE
+     });
 
-var app = builder.Build();
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                 new OpenApiSecurityScheme
+                     {
+                         Reference = new OpenApiReference
+                         {
+                         Type = ReferenceType.SecurityScheme,
+                          Id = AUTHENTICATION_TYPE
+                         },
+                  Scheme = "oauth2",
+            Name = AUTHENTICATION_TYPE,
+            In = ParameterLocation.Header
+        },
+        new List<string>()
+    }
+});
+        var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
 
-app.UseHttpsRedirection();
+        app.UseHttpsRedirection();
 
-app.UseAuthorization();
+        app.UseAuthorization();
 
-app.MapControllers();
+        app.MapControllers();
 
-app.Run();
+        app.Run();
 
-SymmetricSecurityKey SecurityKey()
-{
-    var signinKey = "wMyJ530ueaBBqRksp8rpLQnzIafYZHUa";
+        SymmetricSecurityKey SecurityKey()
+        {
+            var signinKey = "wMyJ530ueaBBqRksp8rpLQnzIafYZHUa";
 
-    var symmetricKey = Encoding.UTF8.GetBytes(signinKey);
+            var symmetricKey = Encoding.UTF8.GetBytes(signinKey);
 
-    return new SymmetricSecurityKey(symmetricKey);
-}
+            return new SymmetricSecurityKey(symmetricKey);
+        }
+    }
